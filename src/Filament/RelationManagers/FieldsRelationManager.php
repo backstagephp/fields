9be3139008e72c86a2351a2c76f6c7eba4d35748
Baspa <10845460+Baspa@users.noreply.github.com>
2025-02-06 12:44:46 +0000

    Fix styling

--- src/Filament/RelationManagers/FieldsRelationManager.php
+++ src/Filament/RelationManagers/FieldsRelationManager.php
@@ -43,7 +43,7 @@ class FieldsRelationManager extends RelationManager
                                     ->required()
                                     ->placeholder(__('Name'))
                                     ->live(debounce: 250)
-                                    ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
 
                                 TextInput::make('slug')
                                     ->readonly(),
@@ -75,10 +75,10 @@ class FieldsRelationManager extends RelationManager
                             ]),
                         Section::make('Configuration')
                             ->columns(3)
-                            ->schema(fn(Get $get) => $this->getFieldTypeFormSchema(
+                            ->schema(fn (Get $get) => $this->getFieldTypeFormSchema(
                                 $get('field_type')
                             ))
-                            ->visible(fn(Get $get) => filled($get('field_type'))),
+                            ->visible(fn (Get $get) => filled($get('field_type'))),
                     ]),
             ]);
     }
