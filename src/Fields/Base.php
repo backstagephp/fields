fa04f3cd51a515460bf4e54204d782a5f3cf69a3
Baspa <10845460+Baspa@users.noreply.github.com>
2025-08-15 09:37:58 +0000

    Fix styling

--- src/Fields/Base.php
+++ src/Fields/Base.php
@@ -2,15 +2,15 @@
 
 namespace Backstage\Fields\Fields;
 
+use Backstage\Fields\Contracts\FieldContract;
 use Backstage\Fields\Models\Field;
-use Filament\Support\Colors\Color;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Grid;
-use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
-use Filament\Forms\Components\ColorPicker;
-use Backstage\Fields\Contracts\FieldContract;
 use Filament\Schemas\Components\Utilities\Get;
+use Filament\Support\Colors\Color;
 
 abstract class Base implements FieldContract
 {
