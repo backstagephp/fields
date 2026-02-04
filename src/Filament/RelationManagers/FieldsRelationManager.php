7db236899d69d748d2e13e9b9e11cbaeb637827e
Casmo <385764+Casmo@users.noreply.github.com>
2026-02-04 08:02:17 +0000

    styles: fix styling issues

--- src/Filament/RelationManagers/FieldsRelationManager.php
+++ src/Filament/RelationManagers/FieldsRelationManager.php
@@ -2,30 +2,30 @@
 
 namespace Backstage\Fields\Filament\RelationManagers;
 
-use Livewire\Component;
-use Filament\Tables\Table;
-use Illuminate\Support\Str;
-use Filament\Schemas\Schema;
-use Filament\Actions\BulkAction;
-use Filament\Actions\EditAction;
+use Backstage\Fields\Concerns\HasConfigurableFields;
+use Backstage\Fields\Concerns\HasFieldTypeResolver;
+use Backstage\Fields\Enums\Field as FieldEnum;
+use Backstage\Fields\Facades\Fields;
 use Backstage\Fields\Models\Field;
+use Filament\Actions\BulkAction;
+use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
-use Filament\Tables\Grouping\Group;
-use Backstage\Fields\Facades\Fields;
-use Filament\Actions\BulkActionGroup;
+use Filament\Actions\EditAction;
 use Filament\Forms\Components\Select;
-use Filament\Schemas\Components\Grid;
-use Filament\Tables\Columns\TextColumn;
-use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
-use Backstage\Fields\Enums\Field as FieldEnum;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Components\Utilities\Set;
-use Backstage\Fields\Concerns\HasFieldTypeResolver;
-use Backstage\Fields\Concerns\HasConfigurableFields;
-use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Grouping\Group;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Livewire\Component;
 
 class FieldsRelationManager extends RelationManager
 {
@@ -154,9 +154,9 @@ class FieldsRelationManager extends RelationManager
             ->groupingSettingsHidden(true)
             ->defaultGroup('group')
             ->groups([
-               'group' => Group::make('group')
+                'group' => Group::make('group')
                     ->titlePrefixedWithLabel(false)
-                    ->getTitleFromRecordUsing(fn(Model|Field $record) => $record->group ?? __('No group'))
+                    ->getTitleFromRecordUsing(fn (Model | Field $record) => $record->group ?? __('No group')),
             ])
             ->columns([
                 TextColumn::make('name')
@@ -169,7 +169,7 @@ class FieldsRelationManager extends RelationManager
                     ->placeholder(__('No Group'))
                     ->searchable()
                     ->sortable()
-                    ->hidden(fn(Table $table) => !$table->isReordering())
+                    ->hidden(fn (Table $table) => ! $table->isReordering())
                     ->getStateUsing(fn (Field $record): string => $record->group ?? __('No Group')),
 
                 TextColumn::make('field_type')
